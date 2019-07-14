4f44e60a02a71b9a52054ce8c84b41ee237d965b
Javier Cuan-Martinez <jcuanmartinez@everquote.com>
2019-07-14 18:13:47 -0400

    Figured out how to add a list dialog box

--- src/screens/AddChocolate/components/Entry.js
+++ src/screens/AddChocolate/components/Entry.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { 
+  FlatList,
   View, 
   Text, 
   TouchableOpacity,
   Dimensions,
  } from 'react-native';
- import DialogInput from 'react-native-dialog-input';
- 
+import DialogInput from 'react-native-dialog-input';
+import Modal from 'react-native-modal';
+
 export default class Entry extends Component {
   constructor(props){
     super(props);
@@ -26,25 +28,48 @@ export default class Entry extends Component {
     this.setState({value: inputText});
   }
 
-  showDialog(isShow){ 
-    this.setState({isDialogVisible: isShow}); 
+  toggleDialogBox(){ 
+    this.setState({ isDialogVisible: !this.state.isDialogVisible }); 
   }
 
   render(){
     return(
-      <TouchableOpacity style={{borderBottomWidth: 0.5, borderBottomColor: "grey"}} onPress={()=>{this.showDialog(true)}}>
+      <TouchableOpacity style={{borderBottomWidth: 0.5, borderBottomColor: "grey"}} onPress={()=>{this.toggleDialogBox()}}>
         <View style={{justifyContent: 'center', width: Dimensions.get('window').width, height: 50, backgroundColor: 'white' }}>
           <Text style={{textAlign: 'left', paddingLeft: 10}}>{this.props.displayName}</Text> 
           <Text style={{textAlign: 'right', paddingRight: 10}}>{this.state.value}</Text>
         </View>
-        <DialogInput isDialogVisible={this.state.isDialogVisible}
+        <Modal 
+          onBackdropPress={() => this.toggleDialogBox()}
+          style={{alignContent: 'center'}} 
+          isVisible={this.state.isDialogVisible}
+        >
+          <View>
+            <FlatList
+              data={[{ key: 'a' }, { key: 'b' }, { key: 'c' }, { key: 'd' }, { key: 'e' }, { key: 'f' }]}
+              renderItem={
+                ({ item }) =>
+                  <TouchableOpacity 
+                    onPress={ () => {
+                      this.sendInput(item.key); 
+                      this.toggleDialogBox(); 
+                    }} 
+                    style={{backgroundColor:'white'}}
+                  >
+                    <Text>{item.key}</Text>
+                  </TouchableOpacity>
+              }
+            />
+          </View>
+        </Modal>
+        {/* <DialogInput isDialogVisible={this.state.isDialogVisible}
           title={this.props.displayName}
           submitInput={ inputText => {
             this.sendInput(inputText); 
-            this.showDialog(false); 
+            this.toggleDialogBox(); 
           }}
-          closeDialog={ () => {this.showDialog(false)}}>
-        </DialogInput>
+          closeDialog={ () => {this.toggleDialogBox()}}>
+        </DialogInput> */}
       </TouchableOpacity>   
     );
   }
