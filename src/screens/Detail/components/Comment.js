f0e44db762bd3cfe67bdaff20d8f0aa8b6a24d79
Javier Cuan-Martinez <jcuanmartinez@everquote.com>
2019-08-03 09:28:28 -0400

    Comments feature added

--- src/screens/Detail/components/Comment.js
+++ src/screens/Detail/components/Comment.js
@@ -10,15 +10,17 @@ import { ListItem } from 'react-native-elements';
 import DbHandler from '../../../helpers/DbHandler';
 import Barcode from '../../../helpers/Barcode';
 import { Colors, Months } from '../../../helpers/Constants';
+import StarRating from 'react-native-star-rating';
 
 export default class Comment extends Component {
     constructor(props){
         super(props);
         this.dbHandler = new DbHandler();
+        this.maxStars = 5;
     }
 
     render(){
-        const { comment, created_ts, displayName } = this.props.item;
+        const { comment, created_ts, displayName, rating} = this.props.item;
         let date_created = created_ts.toDate();
         let date_created_string_format = 
             Months[date_created.getMonth()] + " " +
@@ -30,14 +32,25 @@ export default class Comment extends Component {
                 style={{
                     backgroundColor:"white", 
                     width:Dimensions.get("window").width,
-                    marginBottom:5
+                    marginBottom:8
                 }
             }>
                 <View style={{paddingLeft: 15}}>
                     <Text style={{fontWeight:'bold', paddingBottom: 3}}>
                         {displayName} <Text style={{fontSize: 12, fontWeight:'normal', fontStyle:'italic', color:'rgba(0, 0, 0, .4)'}}> {date_created_string_format} </Text>
                     </Text>
-                    <Text style={{fontSize:12}}>
+
+                    <View style={{paddingRight: Dimensions.get("window").width * .75 }}>
+                        <StarRating
+                            starSize={15}
+                            disabled={true}
+                            maxStars={this.maxStars}
+                            rating={rating}
+                            fullStarColor={"gold"}
+                        />
+                    </View>
+
+                    <Text style={{fontSize:12, paddingBottom: 5}}>
                         {comment}
                     </Text>
                 </View>
