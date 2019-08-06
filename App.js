cc68cebe80b507f8c35af2e022cf610130db3f7f
Javier Cuan-Martinez <jcuanmartinez@everquote.com>
2019-08-06 17:09:47 -0400

    Fixing the famous timer bug with a temprary solution

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react'
-import RootNavigator from './src/navigation/RootNavigator'
+import React, { Component } from 'react';
+import RootNavigator from './src/navigation/RootNavigator';
+import './src/scripts/fixtimerbug';
 
 export default class App extends Component{
   render() { return ( <RootNavigator /> );}
