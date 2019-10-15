1283940330626d165731e132c5d29abdc9d9bd17
Javier Cuan-Martinez <jcuanmartinez@everquote.com>
2019-10-15 14:18:05 -0400

    Created ContactScreen styles

---
+++ src/screens/Contact/styles.js
@@ -0,0 +1,24 @@
+import { Dimensions } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { Colors } from '../../helpers/Constants';
+
+export const ContactScreenStyles = StyleSheet.create({
+    container: {
+        alignItems:'center', 
+        justifyContent:'center',
+        width: Dimensions.get('window').width, 
+        height: Dimensions.get('window').height/3, 
+        backgroundColor: Colors.Secondary,
+    }, 
+    logo: {
+        width:120, 
+        height: 180
+    },
+    text: {
+        paddingRight: 20, 
+        paddingLeft: 20
+    },
+    entry: {
+        fontWeight: 'bold'
+    }
+});
