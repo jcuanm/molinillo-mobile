ed0884e0ac08a3bd07df372b1c0e40d063f0bfd2
Javier Cuan-Martinez <jcuanmartinez@everquote.com>
2019-10-15 11:48:19 -0400

    Created dev enviornment for testing new features

---
+++ assets/Config.js
@@ -0,0 +1,31 @@
+import { 
+  // Firebase Credentials
+  FIREBASE_API_KEY,
+  AUTH_DOMAIN,
+  DATABASE_URL,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID,
+
+  // Algolia Credentials
+  ALGOLIA_API_KEY,
+  ALGOLIA_APP_ID,
+  ALGOLIA_INDEX_NAME,
+} from 'react-native-dotenv';
+
+export const FirebaseConfig = {
+  apiKey: FIREBASE_API_KEY,
+  authDomain: AUTH_DOMAIN,
+  databaseURL: DATABASE_URL,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID
+}
+
+export const AlgoliaSearchConfig = {
+  apiKey: ALGOLIA_API_KEY,
+  applicationID: ALGOLIA_APP_ID,
+  indexName: ALGOLIA_INDEX_NAME
+}
