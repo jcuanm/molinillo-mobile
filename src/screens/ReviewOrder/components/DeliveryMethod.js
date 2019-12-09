bb26c2926373b9fee124577b5a09cc57ea4ca2f0
Javier Cuan-Martinez <jcuanmartinez@everquote.com>
2019-12-08 22:37:36 -0500

    Styled the ReviewOrder page

--- src/screens/ReviewOrder/components/DeliveryMethod.js
+++ src/screens/ReviewOrder/components/DeliveryMethod.js
@@ -6,17 +6,60 @@ import { DeliveryMethodStyles } from '../styles';
 export default class DeliveryMethod extends Component {
 
     render(){
-        const { selectedDeliveryMethod } = this.props;
-        
+        const { selectedDeliveryMethod, shippingAddress } = this.props;
+
         return(
-            <View style={DeliveryMethodStyles.deliveryMethodContainer}>
-                <Text style={DeliveryMethodStyles.deliveryMethodTitle}>
-                    Delivery method:
-                </Text>
-                <Text style={DeliveryMethodStyles.deliveryMethodValue}>
-                    {DeliveryMethodDisplayText[selectedDeliveryMethod]}
-                </Text>
+            <View>
+                {
+                    selectedDeliveryMethod == "shipping" ?
+                        <View style={DeliveryMethodStyles.deliveryMethodContainer}>
+                            <Text style={DeliveryMethodStyles.deliveryMethodTitle}>
+                                Shipping address:
+                            </Text>
+                            <Text style={DeliveryMethodStyles.deliveryMethodValue}>
+                                {this.getAddressString()}
+                            </Text>
+                        </View>
+                    :
+                        <View style={DeliveryMethodStyles.deliveryMethodContainer}>
+                            <Text style={DeliveryMethodStyles.deliveryMethodTitle}>
+                                Delivery method:
+                            </Text>
+                            <Text style={DeliveryMethodStyles.deliveryMethodValue}>
+                                {DeliveryMethodDisplayText[selectedDeliveryMethod]}
+                        </Text>
+                </View>
+                }
             </View>
         );
+    }
+
+    getAddressString(){
+        const {
+            userFullName,
+            streetAddress1,
+            streetAddress2,
+            city,
+            state,
+            zipcode,
+            country,
+        } = this.props.shippingAddress;
+
+        var aggregatedAddress = 
+            userFullName + "\n" +
+            streetAddress1 + "\n";
+        
+        if(streetAddress2 !== ""){
+            aggregatedAddress =  aggregatedAddress + streetAddress2 + "\n";
+        }
+
+        aggregatedAddress = 
+            aggregatedAddress + 
+            city + ", " +
+            state + " " +
+            zipcode + "\n" +
+            country;
+        
+        return aggregatedAddress;
     }
 }
